6ce831a5463f5adb000dcb0dfeeb0c16faaac3f6
Redmond Chan <redmondchan92@gmail.com>
2020-02-11 23:19:11 +0000

    added function to find the day of the week and to calculate monday holidays

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ console.log("Hello")
 
 let holidays = [
     {
+      "name": "Martin Luther King, Jr. Day",
+      "year": 2020,
+      "day": 20,
+      "month": 1 
+    },
+    {
       "name": "Independence Day",
       "year": 2020,
       "day": 4,
@@ -9,3 +15,33 @@ let holidays = [
     }
   ]
 
+function findDay(object){
+  let day = new Date (`${object.month}/${object.day}/${object.year} 12:00:00`)
+
+  return day.getDay()
+}
+
+function mondayHolidays(object, days){
+  let weekends = days/5
+  let begin = (object.day - 2)
+  let end = (object.day) + (weekends * 2) + days
+
+  return `${begin} - ${end}`
+}
+
+mondayHolidays(holidays[0], 5)
+
+// function calculateVacation(object, days){
+//   let weekends = days/5
+//   let begin = 0
+//   let end = 0
+//   if(weekends > 0){
+//     if(object){
+
+//     }
+//   }
+//   return total
+// }
+
+// calculateVacation(holidays[0], 5)
+// findDay(holidays[0])
