ac206279c6a852722789fd9bbf85126429419a0b
Redmond Chan <redmondchan92@gmail.com>
2020-02-16 20:21:28 +0000

    finished function to tackle edge cases and added comments

--- index.js
+++ index.js
@@ -44,144 +44,112 @@ function findDay(object){
 }
 
 function endDate(month, days, year){
- let leapYear = false;
 
- if(year%4 ==0 && year%100 == 0 && year%400 == 0){
-   leapYear = true
- } else if (year%4 == 0 && year%100 != 0){
-   leapYear = true
- }
- console.log(leapYear)
- console.log(days)
+  //checks for next year
+  if(month == 13){
+    month = 1
+    year += 1
+  }
 
- if(month == 2){
-   if(leapYear == true){
-     if(days > 29){
-       days -= 29 
-       month += 1
-       return test(month, days, year)
-     } else {
-       `${month}/${days}/${year}`
-     }
-   } else {
+  //checks for leap year
+  let leapYear = false;
+
+  if(year%4 ==0 && year%100 == 0 && year%400 == 0){
+     leapYear = true
+  } else if (year%4 == 0 && year%100 != 0){
+     leapYear = true
+  }
+
+
+  //checks for feb
+  if(month == 2){
+    if(leapYear == true){
+      if(days > 29){
+        days -= 29 
+        month += 1
+        return endDate(month, days, year)
+      } else {
+       return {"month": month, "day": days, "year": year}
+      }
+    } else {
      if(days > 28){
        days -= 28
        month += 1
-       return test(month, days, year)
+       return endDate(month, days, year)
      } else {
-       return `${month}/${days}/${year}`
+       return {"month": month, "day": days, "year": year}
      }
-   }
- }
-  
-  if([4, 6, 9, 11].indexOf(month) > -1){
-    if(days > 30){
-      days -= 30
-      month += 1
-      return test(month, days, year)
-    }else {
-      return `${month}/${days}`
     }
-  }
+ }
 
+  //checks for months with 30 days
+    if([4, 6, 9, 11].indexOf(month) > -1){
+      if(days > 30){
+        days -= 30
+        month += 1
+        return endDate(month, days, year)
+      }else {
+        return {"month": month, "day": days, "year": year}
+      }
+    }
+
+  //checks for months with 31 days
   if([1,3,5,7,8,10,12].indexOf(month) > -1){
     if(days > 31){
       days -= 31
       month += 1
-      return test(month, days, year)
+      return endDate(month, days, year)
     } else {
-      return `${month}/${days}`
+      return {"month": month, "day": days, "year": year}
     }
   }
 }
 
-function mondayHolidays(object, days){
-  let weekends = days/5
-  let begin = (object.day - 2)
-  let end = (object.day) + (weekends * 2) + days
+function calculate(object1, days){
+  //finds the day of the week the holiday is on
+  let day = findDay(object1)
+  //checks if vacation begins on Monday
+  let begin = 0 
+  if(day == 1){
+    begin = object1.day - 2
+  } else {
+    begin = object1.day
+  }
 
-  return `${begin} - ${end}`
+  //calculates # of weekends
+  let weekends = 0
+  if(days > 4){
+    weekends = Math.floor(days/5)
+  } else {
+    if(day == 2){
+      if(days > 2){
+        weekends = 1
+      }
+    } else if(day == 3){
+      if(days > 1){
+        weekends = 1
+      }
+    } else if (day == 4){
+      if (day > 0){
+        weekends = 1
+      }
+    }
+  }
+
+  //calculates total days from the beginning of the month
+  let totalDays = object1.day + days + (weekends * 2)
+  //calculates the last day of vacation
+  let end = endDate(object1.month, totalDays, object1.year)
+
+
+  //checks if last day lands on a friday
+  if(findDay(end) == 5){
+    end = endDate(end.month, end.day + 2, end.year)
+  }
+
+  return `${object1.month}/${begin}/${object1.year} - ${end.month}/${end.day}/${end.year}`
+  
 }
 
-// function christmasVacation(object1, days){
-//   let weekends = days/5
-//   let begin = 0
-//   let day = findDay(object1)
-//   let result = ""
+calculate(holidays.yr2020[2], 11)
 
-//   if(findDay == 1){
-//     begin = object1.day - 2
-//   } else {
-//     begin = object1.day
-//   }
-//   let end = (object1.day) + (weekends * 2) + days
-//   if(end > 31){
-//     let endYear = object1.year + 1
-//     end -= 31 
-//     end += 1
-
-//     let endMonth = object1.month
-//     if(endMonth == 12){
-//       endMonth = 1
-//     } else {
-//       endMonth += 1
-//     }
-
-//     if([4,6,9,11].indexOf(endMonth) > -1){
-//       if(end > 30){
-//         while(end > 30){
-//           endMonth += 1
-//           end -= 30
-//         }
-//       }
-//     }
-
-//     if([1,3,5,7,8,10,12].indexOf(endMonth) > -1){
-//       if(end > 31){
-//         while(end > 31){
-//           if(endMonth == 12){
-//             endMonth = 1
-//             endYear += 1
-//           } else {
-//             endMonth += 1
-//           }
-//           end -= 31
-//         }
-//       }
-//     }
-
-  
-
-//     //checks if vacation ends on weekend, if it does add the weekend to total duration
-//     let endDay = findDay({"month": 1, "day": end, "year": endYear})
-
-//     if(endDay == 6 || endDay == 0){
-//       end += 2
-//     }
-
-//     return `12/${begin}/${object1.year} - ${endMonth}/${end}/${endYear}`
-//   }
-//   //december always has 31 days
-//   return `12/${begin}/${object1.year} - 2/${end}/${object1.year}`
-// }
-
-christmasVacation(holidays.yr2020[3], 30)
-// let x = findDay(holidays.yr2019[0])
-// console.log(x)
-
-// mondayHolidays(holidays[2], 5)
-
-// function calculateVacation(object, days){
-//   let weekends = days/5
-//   let begin = 0
-//   let end = 0
-//   if(weekends > 0){
-//     if(object){
-
-//     }
-//   }
-//   return total
-// }
-
-// calculateVacation(holidays[0], 5)
-// findDay(holidays[0])
